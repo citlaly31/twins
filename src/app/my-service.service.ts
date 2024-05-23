c88d2b28b9bc337c38f8c518b7a6ebea3cf3ddbe
HeribertoGS <gijon.sanchez501@gmail.com>
2024-05-23 08:20:44 -0600

    Agregando servicio

---
+++ src/app/my-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyService {
+
+  private apiUrl1 = 'https://jsonplaceholder.typicode.com/posts';
+  private apiUrl2 = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  // Petición para obtener posts
+  getPosts(): Observable<any> {
+    return this.http.get(this.apiUrl1);
+  }
+
+  // Petición para obtener usuarios
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiUrl2);
+  }
+}
