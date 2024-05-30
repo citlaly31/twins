363d21a6a5f1b722fd7e109392623307c5ea8961
HeribertoGS <gijon.sanchez501@gmail.com>
2024-05-30 07:48:20 -0600

    Crud-usuarios

---
+++ src/app/crud-usuario/crud-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-usuario.component.html',
+  styleUrl: './crud-usuario.component.css'
+})
+export class CrudUsuarioComponent {
+
+}
